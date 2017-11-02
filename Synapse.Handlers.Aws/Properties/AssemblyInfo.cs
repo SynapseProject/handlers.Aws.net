9b5a1e0a307e5b19286148f6abf77d076383e439
yangz1 <yeongtze@googlemail.com>
2017-11-02 04:14:23 -0400

    Added script to set assembly version.

--- Synapse.Handlers.Aws/Properties/AssemblyInfo.cs
+++ Synapse.Handlers.Aws/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Synapse.Handlers.Aws")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct("Synapse.Handlers.Aws")]
-[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2017")]
+[assembly: AssemblyCopyright( "" )]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "0.1.0.0" )]
+[assembly: AssemblyFileVersion( "0.1.17306.0" )]
